3bf0ed787c101a15657ed452c559989ae613c992
afnanalotaibi0 <aalotaibi@kabi.ai>
2024-10-19 22:13:34 +0300

    hero path

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 import Header from "./components/Header";
-import Hero from "./components/HowItWorks";
+import Hero from "./components/Hero";
 import About from "./components/About";
 import Footer from "./components/Footer";
 import HowItWorks from "./components/HowItWorks";
