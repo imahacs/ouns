49f4f30a9f54b6953cb2aa7ccc89eac40bf39295
MAHA <mem880.a@gmail.com>
2024-10-20 23:43:51 +0300

    Add: style chsnges

--- public/assets/assets.js
+++ public/assets/assets.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg'; 
 import header from './header.svg'; 
 import hero from './hero.png';
+import thx from "./images/thanku.png"
+import cam from "./images/cam.png"
+import audio from "./images/audio.png"
 
 
-export { logo, header, hero }; 
+export { logo, header, hero, thx, cam, audio }; 
